2952cba18c012a85c9d6ae6dc59e0264e838fb90
TakumiTakanoTMCIT <m23094@g.metro-cit.ac.jp>
2025-09-25 15:47:04 +0900

    feat(player): 回転するようになりました -PlayerCtrlクラスに追記

--- Assets/MyAsset/Script/Controllers/PlayerCtrl.cs
+++ Assets/MyAsset/Script/Controllers/PlayerCtrl.cs
@@ -5,6 +5,9 @@ public class PlayerCtrl : MonoBehaviour
 {
     [SerializeField] private InputActionReference _moveAction; // Inspectorでアサイン
 
+    [SerializeField]
+    private float _rotateSpeed = 0.5f;
+
     public float speed = 5f;
 
     private void OnEnable()
@@ -22,5 +25,18 @@ public class PlayerCtrl : MonoBehaviour
         Vector2 moveInput = _moveAction.action.ReadValue<Vector2>();
         Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
         transform.position += move * speed * Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            //同時押しはなにもしない
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -_rotateSpeed * Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, _rotateSpeed * Time.deltaTime, 0);
+        }
     }
 }
