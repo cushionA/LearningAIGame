5745695d1c9a9004bf31d04316f52e23d2ee64a7
TakumiTakanoTMCIT <m23094@g.metro-cit.ac.jp>
2025-09-25 15:49:07 +0900

    feat(player): 移動方法をInputクラスでするように変更しました。 -PlayerCtrlに追記

--- Assets/MyAsset/Script/Controllers/PlayerCtrl.cs
+++ Assets/MyAsset/Script/Controllers/PlayerCtrl.cs
@@ -1,32 +1,35 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    [SerializeField] private InputActionReference _moveAction; // Inspectorでアサイン
-
     [SerializeField]
     private float _rotateSpeed = 0.5f;
 
     public float speed = 5f;
 
-    private void OnEnable()
-    {
-        _moveAction.action.Enable();
-    }
-
-    private void OnDisable()
-    {
-        _moveAction.action.Disable();
-    }
-
     private void Update()
     {
-        Vector2 moveInput = _moveAction.action.ReadValue<Vector2>();
-        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
-        transform.position += move * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
 
-        if(Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
         {
             //同時押しはなにもしない
         }
